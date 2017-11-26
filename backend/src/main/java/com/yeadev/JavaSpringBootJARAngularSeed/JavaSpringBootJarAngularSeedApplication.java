742b7b057ff42b70fe2720c06c085553710e8c3f
Jeff Tham <jeff.tham@live.com>
2017-11-26 14:50:04 -0500

    working seed linked java backend and angular frontend

---
+++ backend/src/main/java/com/yeadev/JavaSpringBootJARAngularSeed/JavaSpringBootJarAngularSeedApplication.java
@@ -0,0 +1,50 @@
+package com.yeadev.JavaSpringBootJARAngularSeed;
+
+import java.util.Collections;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+public class JavaSpringBootJarAngularSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootJarAngularSeedApplication.class, args);
+    }
+
+    // map all unknown path to angular frontend, let angular to handle it.
+    @Bean
+    public ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return new ErrorViewResolver() {
+            @Override
+            public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+                return status == HttpStatus.NOT_FOUND
+                        ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+                        : null;
+            }
+        };
+    }
+
+    // the following code is not needed, it just output the name of the bean which bundle in Spring Boot
+    /*
+        @Bean
+        public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+            return args -> {
+
+                System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println("beanName = " +beanName);
+                }
+
+            };
+        }
+     */
+}
---
+++ backend/src/main/java/com/yeadev/JavaSpringBootJARAngularSeed/controllers/MainController.java
@@ -0,0 +1,31 @@
+package com.yeadev.JavaSpringBootJARAngularSeed.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    // sample code: return string value through http request/respond
+    @RequestMapping("/returnString")
+    public String returnString() {
+        return "Greetings from Spring Boot!";
+    }
+
+    // ref : https://www.java2blog.com/spring-restful-web-services-json-example/
+    // if headers = "Accept=application/json", jackson will map it
+    @RequestMapping(value = "/returnJson", method = RequestMethod.GET, headers = "Accept=application/json")
+    public List returnJson1() {
+        List<String> listOfCountries = new ArrayList<>();
+        listOfCountries = Arrays.asList("US", "UK");
+        return listOfCountries;
+    }
+
+
+
+}
