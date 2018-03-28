545ae520e06b62ead673221d61c14a8f979e3450
Jeff Tham <jeff.tham@live.com>
2018-03-27 22:38:04 -0400

    added in memory Spring Security for several links

---
+++ backend/src/main/java/com/yeadev/JavaSpringBootJARAngularSeed/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,77 @@
+package com.yeadev.JavaSpringBootJARAngularSeed.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // visitors can access all except ...
+        http
+            .authorizeRequests()
+            .antMatchers("/jdbc/**","/returnJson").authenticated()
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            //.loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+
+        /* // other example
+         http.authorizeRequests()
+			.antMatchers("/").hasRole("EMPLOYEE")
+			.antMatchers("/leaders/**").hasRole("MANAGER")
+			.antMatchers("/systems/**").hasRole("ADMIN")
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
+		*/
+    }
+
+    /*
+        @Bean
+        @Override
+        public UserDetailsService userDetailsService() {
+            UserDetails user =
+                    withDefaultPasswordEncoder()
+                            .username("user")
+                            .password("password")
+                            .roles("USER")
+                            .build();
+
+            return new InMemoryUserDetailsManager(user);
+        }
+    */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add our users for in memory authentication
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("a").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("b").password("test123").roles("EMPLOYEE", "MANAGER"))
+                .withUser(users.username("c").password("test123").roles("EMPLOYEE", "ADMIN"));
+
+        // use database
+        // auth.jdbcAuthentication().dataSource(securityDataSource);
+    }
+
+}
